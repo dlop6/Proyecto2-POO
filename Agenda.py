bb97a64c5e8d5da4be1728fdefcad9c56c62c145
Diego L <lop23747@uvg.edu.gt>
2023-11-22 16:58:21 -0600

    si

--- Agenda.py
+++ Agenda.py
@@ -1,16 +1,3 @@
 import tkinter as tk
 from  tkcalendar import Calendar
 
-def on_select_date(date):
-    print("Fecha seleccionada:", date)
-
-root = tk.Tk()
-root.title("Calendario de Ejemplo")
-
-cal = Calendar(root, selectmode="day", year=2023, month=11, day=7)
-cal.pack(pady=20)
-
-btn = tk.Button(root, text="Seleccionar Fecha", command=lambda: on_select_date(cal.get_date()))
-btn.pack(pady=20)
-
-root.mainloop()
